499df16068b4438da81f3f5406b42d9112c0b962
PROG WEB - MANHA <PROG WEB - MANHA@MININT-633QJ9F>
2021-01-13 12:51:49 -0200

    Arquivos base inicio

--- paginas/inicio.php
+++ paginas/inicio.php
@@ -1,8 +1,100 @@
-<?php
+ 
+<div class="container my-5 ">
+    <div class="row">
+        <div class="col">
+            <div class="row">
+                <img src="assets/img/imgheader1.jpg" alt=""/>
+            </div>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        </div>
+        <div class="col text-black">
+            <h1>Mussum Pissium</h1>
+            <p class="text-justify"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting, 
+                remaining essentially unchanged. It was popularised in the 1960s with the release ocimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting, 
+                remaining essentially unchanged. It was popularised in the 1960s with the release ocimen book. 
+                It has survived not only five centuries, but also the leap into ease ocimen book. 
+                It has survived not only five centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. </p>
+        </div>
+    </div>
+    <hr>
 
+    <div class="row col mr-0">
+        <div class="col">
+            <img class="img-fluid" src="assets/img/imginicial2.jpg.jpg" alt=""/>
+        </div>
+        <div class="col">
+            <img class="img-fluid" src="assets/img/imginicial3.jpg" alt=""/>
+        </div>
+        <div class="col">
+            <img class="img-fluid" src="assets/img/imginicial4.jpg" alt=""/>
+        </div>
+        <div class="col">
+            <img class="img-fluid" src="assets/img/imginicial5.jpg" alt=""/>
+        </div>
+        <div class="col">
+            <img class="img-fluid" src="assets/img/imginicial6.jpg" alt=""/>
+        </div>
+        <div class="col">
+            <img class="img-fluid" src="assets/img/imginicial7.jpg" alt=""/>
+        </div>
+
+    </div>
+    <div class="row p-3 text-black">
+        <div class="col bordas-laterais text-center px-5 py-4">
+            <h2>Mussum Pissium</h2>
+            <p class="text-justify">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            <button class="btn btn-success"><span class=" Juli-zoom-in"></span> Ver mais</button>
+        </div>
+        <div class="col bordas-laterais text-center px-5 mx-3 py-4 text-black">
+            <h2>Mussum Pissium</h2>
+            <p class="text-justify">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            <button class="btn btn-success"><span class=" Juli-zoom-in"></span> Ver mais</button>
+
+        </div>
+    </div>
+    <div class="row p-3">
+        <div class="col bordas-laterais text-center px-5 py-4">
+            <h2>Mussum Pissium</h2>
+            <p class="text-justify">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            <button class="btn btn-success"><span class=" Juli-zoom-in"></span> Ver mais</button>
+        </div>
+        <div class="col bordas-laterais text-center px-5 mx-3 py-4 text-black">
+            <h2>Mussum Pissium</h2>
+            <p class="text-justify">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            <button class="btn btn-success"><span class=" Juli-zoom-in"></span> Ver mais</button>
+        </div>
+        
+    </div>
+     <div class="row p-3">
+        <div class="col bordas-laterais text-center px-5 py-4">
+            <h2>Mussum Pissium</h2>
+            <p class="text-justify">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            <button class="btn btn-success"><span class=" Juli-zoom-in"></span> Ver mais</button>
+        </div>
+        <div class="col bordas-laterais text-center px-5 mx-3 py-4 text-black">
+            <h2>Mussum Pissium</h2>
+            <p class="text-justify">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            <button class="btn btn-success"><span class=" Juli-zoom-in"></span> Ver mais</button>
+        </div>
+        
+    </div>
+    <div>
+    </div>
+</div>
