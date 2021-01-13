5d5cd04c038edcba28642275227174c0b739ad50
PROG WEB - MANHA <PROG WEB - MANHA@MININT-633QJ9F>
2021-01-13 11:16:19 -0200

    Arquivos base

---
+++ paginas/includes/footer.php
@@ -0,0 +1,31 @@
+ <footer class="container text-center text-secondary">
+           <div class="row text-center">
+                <div class="col  bordas-laterais py-4 mb-5 border border-success">
+                    <ul class="list-group rodape border border-success">
+                        <li class="list-group-item"><button class="btn btn-success btn-block px-5 arredondamento"><span class="reg-plus"></span> Ver mais</button></li>
+                        <li class="list-group-item"><button class="btn btn-success btn-block px-5 arredondamento"><span class="reg-plus"></span> Ver mais</button></li>
+                        <li class="list-group-item"><button class="btn btn-success btn-block px-5 arredondamento"><span class="reg-plus"></span> Ver mais</button></li>
+                        <li class="list-group-item"><button class="btn btn-success btn-block px-5 arredondamento"><span class="reg-plus"></span> Ver mais</button></li>
+                        
+                    </ul>
+                </div>
+                
+                <div class="col text-center bordas-laterais text-center px-5 mx-3 my-3 py-4 text-white">
+                    <img class="img-fluid arredondamento-img border border-success" src="http://placehold.it/100x100" alt=""/>
+                    <h5 class="py-3"> Lorem Ipsum</h5>
+                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        </p>
+                </div>
+                
+                <div class="col bordas-laterais py-4 mb-5 border border-success">
+                    <img
+            </div>
+        </div>
+        </footer>
+
+        <!--Scripts => js-->
+        <!--jquery-->
+        <script src="assets/js/jquery.js"></script>
+        <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ paginas/includes/header.php
@@ -0,0 +1,54 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Contato</title>
+
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="plugins/Icomoon/Icomoon.css" />
+        <link rel="stylesheet" href="assets/css/estilosite.css" />
+        <link rel="stylesheet" href="assets/img/" />
+    </head>
+    <body>
+
+        <header class="container col-sm ">
+            <!--!--cabeçalho-->
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="assets/img/imgheader1.jpg" alt="Primeiro Slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="assets/img/imgheader2.jpg" alt="Segundo Slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="assets/img/imgheader3.jpg" alt="Terceiro Slide">
+                    </div>
+                </div>
+            </div>
+            <div><img src="" alt="" />
+                <!--navegação-->
+               <nav class="navbar navbar-expand-lg navbar-primary bg-primary">
+<!--                    <a class="navbar-brand" href="#">Navbar</a>-->
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active"><a class="nav-link" href="?pg=inicio">Início <span class="sr-only">(página atual)</span></a></li>
+                            <li class="nav-item active"><a class="nav-link" href="?pg=sobre">Sobre <span class="sr-only">(página atual)</span></a></li>
+                            <li class="nav-item active"><a class="nav-link" href="?pg=servicos">Grades <span class="sr-only">(página atual)</span></a></li>
+                            <li class="nav-item active"><a class="nav-link" href="?pg=produtos">Novidades <span class="sr-only">(página atual)</span></a></li>
+                            <li class="nav-item active"><a class="nav-link" href="?pg=contato">Contato <span class="sr-only">(página atual)</span></a></li>
+                            
+                            
+                            
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+                        </form>
+                    </div>
+                </nav>
+            </div> <!-- fim navegação-->
+        </header> 
