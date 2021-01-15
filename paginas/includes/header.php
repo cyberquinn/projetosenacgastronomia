3160d4ac54f6ad44537ebe94264a8d892adad731
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-K37320V>
2021-01-15 12:10:36 -0300

    Redmencionamento da imagem do header (logo Senac))) ATT: Mudança no HEADER

--- paginas/includes/header.php
+++ paginas/includes/header.php
@@ -33,62 +33,64 @@
 
                     <div class="col-4"></div>
                 </div>
-                <nav class="navbar  navbar-expand-lg navbar-info">
-                    <img src="assets/img/logo_senac.png" alt="" class="img-fluid ">
-                    <a class="navbar-brand" href="#"></a>
-                    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="?pg=inicio"> <span class=""> </span> Inicial </a></li>
-                            <li class="nav-item active">
-                                <a class="nav-link " href="?novidades"> <span class=""> </span> Destaques </a></li>
-                            <li class="nav-item active">
-                                <a class="nav-link " href="?pg=sobre"><span class=""></span>Sobre</a></li>
-                            <li class="nav-item active">
-                                <a class="nav-link " href="?pg=contato"> <span class=""> Contato</span></a></li>
-
-                            <nav class="social">
-                                <a href="https://www.facebook.com/SenacBrasil" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                <a href="https://www.instagram.com/senacbrasil/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                                <a href="https://twitter.com/SenacBrasil" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                <a href="http://www.linkedin.com/company/senac-brasil" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                                <a href="https://www.youtube.com/user/SenacNacional" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-                            </nav>
-                        </ul>
+                <div class="row">
+                    <div class="col">                   
+                        <nav class="navbar  navbar-expand-lg navbar-info">
+                            <img src="assets/img/logo_senac.png" alt="" class="img-fluid ">
+                            <a class="navbar-brand" href="#"></a>
+                            <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                                <ul class="navbar-nav mr-auto ">
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="?pg=inicio"> <span class=""> </span> Inicial </a></li>
+                                    <li class="nav-item active">
+                                        <a class="nav-link " href="?novidades"> <span class=""> </span> Destaques </a></li>
+                                    <li class="nav-item active">
+                                        <a class="nav-link " href="?pg=sobre"><span class=""></span>Sobre</a></li>
+                                    <li class="nav-item active">
+                                        <a class="nav-link " href="?pg=contato"> <span class=""> Contato</span></a></li>
+                                </ul>
+                        </nav>
                     </div>
-                </nav>
 
-                <hr>
-
-                <!-- Header -->
-
-
-            </div>
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="assets/img/IMG3.jpg" alt="Primeiro Slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="assets/img/IMG2.jpg" alt="Segundo Slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="assets/img/IMG1.jpg" alt="Terceiro Slide">
+                    <div class="col"><nav class="social text-right m-4">
+                            <a href="https://www.facebook.com/SenacBrasil" target="_blank"><i class="Juli-facebook2" aria-hidden="true"></i></a>
+                            <a href="https://www.instagram.com/senacbrasil/" target="_blank"><i class=" Juli-instagram1" aria-hidden="true"></i></a>
+                            <a href="https://twitter.com/SenacBrasil" target="_blank"><i class="Juli-twitter1" aria-hidden="true"></i></a>
+                            <a href="https://www.youtube.com/user/SenacPernambuco" target="_blank"><i class="Juli-youtube2" aria-hidden="true"></i></a>
+                        </nav> 
                     </div>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Anterior</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Próximo</span>
-                </a>
+            </div>  
+        </div>
+        <hr>
+
+        <!-- Header -->
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="assets/img/IMG3.jpg" alt="Primeiro Slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="assets/img/IMG2.jpg" alt="Segundo Slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="assets/img/IMG1.jpg" alt="Terceiro Slide">
+                </div>
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Anterior</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Próximo</span>
+            </a>
+        </div>
     </body>
 </html>
