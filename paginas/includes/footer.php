82cd133f988344d313865abee82be1e9ad9f0c02
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-K37320V>
2021-01-15 12:11:22 -0300

    Atualização no footer (ícones)

--- paginas/includes/footer.php
+++ paginas/includes/footer.php
@@ -9,15 +9,13 @@
             <h3>Senac | Departamento Nordeste</h3>
         </div>
     </div>
-    <div class="linha copy">
         <div class="container">
             <span><img src="https://www.dn.senac.br/wp-content/themes/senac/images/senac_logo_branco.png" alt="Senac" /> © Todos os Direitos Reservados - 2017.</span>
             <nav class="social">
-                <a href="https://www.facebook.com/SenacBrasil" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                <a href="https://www.instagram.com/senacbrasil/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                <a href="https://twitter.com/SenacBrasil" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                <a href="https://www.linkedin.com/company/2655383" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                <a href="https://www.youtube.com/user/SenacNacional" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                <a href="https://www.facebook.com/SenacBrasil" target="_blank"><i class="Juli-facebook2" aria-hidden="true"></i></a>
+                <a href="https://www.instagram.com/senacbrasil/" target="_blank"><i class=" Juli-instagram1" aria-hidden="true"></i></a>
+                <a href="https://twitter.com/SenacBrasil" target="_blank"><i class="Juli-twitter1" aria-hidden="true"></i></a>
+                <a href="https://www.youtube.com/user/SenacNacional" target="_blank"><i class="Juli-youtube2" aria-hidden="true"></i></a>
             </nav>
         </div>  
     </div>
